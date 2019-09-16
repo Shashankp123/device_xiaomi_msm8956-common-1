5a23a7bb0dab1714df8f9af8d68ef99978a3af43
Sagarrokade006 <sagarrokade006@gmail.com>
2019-09-17 00:23:55 +0530

    Drop Thermal

--- msm8956.mk
+++ msm8956.mk
@@ -395,10 +395,10 @@ PRODUCT_PACKAGES += \
     textclassifier.zh-Hant.model
 
 # Thermal
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.msm8952
+#PRODUCT_PACKAGES += \
+#    android.hardware.thermal@1.0-impl \
+#    android.hardware.thermal@1.0-service \
+#    thermal.msm8952
 
 # USB HAL
 PRODUCT_PACKAGES += \
